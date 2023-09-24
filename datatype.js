3192697b3f1563329d7d442924724ee36f91441f
ankit <ankitsingharmy639@gmail.com>
2023-09-24 15:55:57 +0530

    datatype revision day 2

---
+++ datatype.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+//numer => 2 to power 53
+// BigInt
+// string =>""
+// Boolean =>true /false
+// null=> standalone value ya jiski value abhi assign nhi hua hai and NULL is object tye 
+// undefined =>when value is not defined and it type is undefined 
+// Symbol =>it is kmown for uniqueness
+
+// object
+
+//  let age =34;
+//  console.log(typeof(age))
+
+//console.log(typeof(null)) 
+//object
+//console.log(typeof(undefined)) 
+//undefined
