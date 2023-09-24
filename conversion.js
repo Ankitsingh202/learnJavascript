b323dc9c77659f11a8a000e340129c237e524502
ankit <ankitsingharmy639@gmail.com>
2023-09-24 16:31:07 +0530

    basic conversion

---
+++ conversion.js
@@ -0,0 +1,16 @@
+// let value =332
+// let value1 = "32"
+// let value2 = "23asn"
+//  console.log(value)
+//  console.log(value1)
+//  console.log(value2)
+
+//  console.log(typeof(value2))
+
+// CONVERTING NUMBER TO STRING 
+
+// let somenumber = 23
+// let stringNumber = String(somenumber)
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
