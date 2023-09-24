3c45a0953833579c9f63650b08fafb48af19fc1e
ankit <ankitsingharmy639@gmail.com>
2023-09-24 15:20:14 +0530

    lets start from root

---
+++ let_const_var.js
@@ -0,0 +1,10 @@
+var name = "anurag singh"
+const age = 23
+let clgName = "jaipur national university"
+// this is used to print onr by one on console
+// console.log(name)
+// console.log(age)
+
+// while printing all we use console.table
+console.table([name,age,clgName])
+//var use karne is scope (block scope and functional scope) wala problem  bahut ata tha tab se hum log var ko use nhi krte hai phir humlog let ko se use karte hai
