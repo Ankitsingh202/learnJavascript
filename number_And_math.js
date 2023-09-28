778c1e5ac6fc2fb9eaa6d0db8552fad20d9f6db2
ankit <ankitsingharmy639@gmail.com>
2023-09-28 08:21:13 +0530

    basic number and math

---
+++ number_And_math.js
@@ -0,0 +1,26 @@
+// let score = 400
+// const balance = new Number(100)
+//  console.log(balance)
+//  console.log(score)
+//  console.log(balance.toString().length)
+//  console.log(balance.toFixed(4)) //it is used to add number of zeroes after the number 
+//  const otherNumber =34.545
+//  console.log(otherNumber.toPrecision(3)) // it show only three output 
+//  const hunderd = 10000000000;
+//  console.log(hunderd.toLocaleString())//by usa standard
+//  console.log(hunderd.toLocaleString('en-IN'))
+
+// +++++++math++++++
+console.log(Math)
+console.log(Math.abs(-232))
+console.log(Math.round(23.45))
+console.log((Math.round(54.87)))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(34.8))
+console.log(Math.min(23,343,5343,4))
+console.log(Math.random())//yaha 0 se 1 ke bichh ka value aayega 
+console.log((Math.random()*10)+1)//yaha 1 se 10 ke bichh ka value aayega 
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+console.log(Math.floor(Math.random()*10)+min)
