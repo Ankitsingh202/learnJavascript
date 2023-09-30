563e5259424c7652fff07f56bdfffdd66cf7341d
ankit <ankitsingharmy639@gmail.com>
2023-09-30 12:12:01 +0530

    api

--- oobjects destructuring and jason api.js
+++ oobjects destructuring and jason api.js
@@ -1,1 +1,17 @@
-j
+const course={
+    cousreName :"js in hindi",
+    price:"99/-",
+    couserInstructure:"Ankit singh"
+} 
+
+const {couserInstructure:instructor} = course//here destracture the the the course into courseaInstructor and give its name as instructor
+//console.log(couserInstructure);
+console.log(instructor)//here fecting ny using the another name of cousreinstructure is instructor
+
+// json ka koee name nhi hota hai jab isko banayenge to key  ko string ke formate me likhna hoga 
+// for exampole api in object form
+// {
+//     "name":"anurag singh",
+//     "coursename":"js in hindi",
+//     "price":"free"
+// }
