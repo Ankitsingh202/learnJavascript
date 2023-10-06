1627a8a7e2100ec11186fa3b6ef1ce06b978dd79
ankit <ankitsingharmy639@gmail.com>
2023-10-06 22:48:57 +0530

    arrow function

---
+++ arrowfunction.js
@@ -0,0 +1,17 @@
+
+// what is difference between arrow and normal functions 
+
+// const addTwo=(num1,num2)=>{
+//  return num1+num2
+// }
+//or
+const addTwo=(num1,num2)=>num1+num2//implicite 
+   
+console.log(addTwo(2,5))
+
+
+
+
+
+
+// andar ka function jab bahar ka fuction ko access karta hai toh usko closer bolte hai 
---
+++ scope.js
@@ -0,0 +1,13 @@
+// function one(){
+//     const username ="anurag singh"
+//     function two(){
+//         const website = "youtube"
+//         console.log(username)
+//     }
+//     console.log(website)
+//     two()
+// }
+// one()
+
+
+// andar ka function jab bahar ka fuction ko access karta hai toh usko closer bolte hai 
